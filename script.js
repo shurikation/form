d57d94bd9bf299133253a77ed2250720c886798c
shurikation <shurikation@gmail.com>
2021-01-27 22:07:03 +0400

    refact: format script and styles file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 //c 45 minute!
-document.addEventListener('DOMContentLoaded', function () {
-	const form = document.getElementById('form');
-	form.addEventListener('submit', formSend);
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("form");
+	form.addEventListener("submit", formSend);
 
 	async function formSend(event) {
 		event.preventDefault();
@@ -11,94 +11,94 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		//Отправка формы!
 		let formData = new FormData(form); //Вытягивает данные полей
-		formData.append('image', formImage.files[0]); //..+инфа изображение
+		formData.append("image", formImage.files[0]); //..+инфа изображение
 
 		//Если форма прошла валидацию, то:
 		if (error === 0) {
-			form.classList.add('_sending'); //Показываем юзеру, что отправка началась...
+			form.classList.add("_sending"); //Показываем юзеру, что отправка началась...
 
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
+			let response = await fetch("sendmail.php", {
+				method: "POST",
+				body: formData,
 			});
 
-			if(response.ok) {
+			if (response.ok) {
 				let result = await response.json();
 				alert(result.message);
 				formPreview.innerHTML = ""; //Очистка полей форм
-            form.reset(); //Очистка полей форм
-            form.classList.remove('_sending');
+				form.reset(); //Очистка полей форм
+				form.classList.remove("_sending");
 			} else {
-            alert('Error response!');
-            form.classList.remove('_sending');
-         }
-         
+				alert("Error response!");
+				form.classList.remove("_sending");
+			}
 		} else {
-         console.log(error);
-			alert('Zapolni!')
+			console.log(error);
+			alert("Zapolni!");
 		}
 	}
 
-
 	function formValidate(form) {
 		let error = 0;
-		let formReq = document.querySelectorAll('._req');
+		let formReq = document.querySelectorAll("._req");
 
 		for (let index = 0; index < formReq.length; index++) {
 			const input = formReq[index];
 			formRemoveError(input);
 
-			if (input.classList.contains('_email')) {
+			if (input.classList.contains("_email")) {
 				if (emailTest(input)) {
 					formAddError(input);
 					error++;
 				}
-			} else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+			} else if (
+				input.getAttribute("type") === "checkbox" &&
+				input.checked === false
+			) {
 				formAddError(input);
 				error++;
 			} else {
-				if (input.value === '') {
+				if (input.value === "") {
 					formAddError(input);
 					error++;
 				}
 			}
-      }
-      return error;
+		}
+		return error;
 	}
 
-
 	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
+		input.parentElement.classList.add("_error");
+		input.classList.add("_error");
 	}
 
 	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
+		input.parentElement.classList.remove("_error");
+		input.classList.remove("_error");
 	}
 
 	function emailTest(input) {
 		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 	}
 
-	//Вывод миниатюр
-	const formImage = document.getElementById('formImage');
-	const formPreview = document.getElementById('formPreview');
+	//! Вывод миниатюр
+	const formImage = document.getElementById("formImage");
+	const formPreview = document.getElementById("formPreview");
 
-	formImage.addEventListener('change', () => {
+	formImage.addEventListener("change", () => {
 		uploadFile(formImage.files[0]);
 	});
 
 	//Проверки
 	function uploadFile(file) {
-		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-			alert('Images only approved!');
-			formImage.value = '';
+		if (!["image/jpeg", "image/png", "image/gif"].includes(file.type)) {
+			alert("Images only approved!");
+			formImage.value = "";
 			return;
 		}
 
 		if (file.size > 2 * 1024 * 1024) {
-			alert('File must be no more 2MB')
+			alert("File must be no more 2MB");
 		}
 
 		var reader = new FileReader();
@@ -106,9 +106,8 @@ document.addEventListener('DOMContentLoaded', function () {
 			formPreview.innerHTML = `<img src="${e.target.result}" alt="foto">`;
 		};
 		reader.onerror = function (e) {
-			alert('Error!');
+			alert("Error!");
 		};
 		reader.readAsDataURL(file);
 	}
-
 });
